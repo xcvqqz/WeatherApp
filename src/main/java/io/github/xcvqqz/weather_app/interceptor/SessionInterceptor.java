15f553b65e04d9f38e0df89ff53ebefc77a06338
MaximMaslov <grossholder77@gmail.com>
2026-08-16 15:02:20 +0300

    ref: 1) fix app_key name for application.properties;
    2) fixed session retrieval and validation of the session within the SessionInterceptor class;

--- src/main/java/io/github/xcvqqz/weather_app/interceptor/SessionInterceptor.java
+++ src/main/java/io/github/xcvqqz/weather_app/interceptor/SessionInterceptor.java
@@ -1,9 +1,11 @@
 package io.github.xcvqqz.weather_app.interceptor;
 
 
+import io.github.xcvqqz.weather_app.exception.SessionNotFoundException;
 import io.github.xcvqqz.weather_app.service.CookieService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,25 +16,23 @@ import java.util.UUID;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class SessionInterceptor implements HandlerInterceptor {
 
-    @Autowired
-    private CookieService cookieService;
+    private final CookieService cookieService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
 
-        UUID userSessionId = cookieService.getSessionId(request);
-
-        if(userSessionId == null){
+        try {
+            UUID userSessionId = cookieService.getSessionId(request);
+            log.info("Session with UUID {} was received", userSessionId);
+            request.setAttribute("userSessionId", userSessionId);
+            return true;
+        } catch (SessionNotFoundException e) {
             log.warn("User session Id is null, redirect to sign-in is needed to establish a session.");
             response.sendRedirect("/sign-in");
             return false;
         }
-
-        log.info("Session successfully created with UUID: {} ", userSessionId);
-        request.setAttribute("userSessionId", userSessionId);
-
-        return true;
     }
 }
